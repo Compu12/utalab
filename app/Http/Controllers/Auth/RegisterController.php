3cb733d3092c91b6a60b0889d5f12344dbd676e2
Xavier Cañizares <xcanizar@vinarytech.com>
2022-01-25 14:53:44 -0500

    conexion sqlsrv

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,7 +57,7 @@ class RegisterController extends Controller
             'canton' => ['required', 'string', 'max:255'],
             'direccion' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
     }
 
