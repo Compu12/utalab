cee8eaaa33e625cd53f35c6fd67f2c62b218dc95
RichardC <rcaiza5293@gmail.com>
2022-01-28 21:25:35 -0500

    MENSAJE

--- resources/views/solicitudInterna/index.blade.php
+++ resources/views/solicitudInterna/index.blade.php
@@ -9,7 +9,7 @@
     <div class="card-header py-2">
         <div class="row">
             <div class="col-md-6 ">
-                <h6 class="m-0 font-weight-bold text-primary mt-2">Administración de solicitudes internas de análisis</h6>
+                <h6 class="m-0 font-weight-bold text-primary mt-2">Administración de solicitudes internas de análisis HOLA</h6>
             </div>
             <div class="col-md-6 text-right">
                 <button type="button" class="btn btn-outline-primary text-right" onclick="guardar()">
